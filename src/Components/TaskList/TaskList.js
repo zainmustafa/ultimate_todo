c5365e963a12db7e9b446355c8e9e11f0dbb1592
Osama Khan <osama.ahmedkhan96@gmail.com>
2018-09-17 17:05:19 +0500

    delete task functionality added

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
 import { Button, Checkbox, Table} from 'react-bootstrap';
+import db from "../../Config/config";
 
 class TaskList extends Component{
 
     constructor(porps){
         super(porps);
+        this.delTask = this.delTask.bind(this);
+    }
+
+    delTask(id){
+        console.log(id)
+        db.collection("task").doc(id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
     }
 
     render(){
         const {todoList} = this.props;
-        console.log(todoList)
         return(
             <Table responsive>
                 <thead>
@@ -22,13 +32,13 @@ class TaskList extends Component{
                 </tr>
                 </thead>
                 <tbody>
-                {todoList.map(function (todo, index) {
+                {todoList.map((todo, index) => {
                     return <tr key={todo.id}>
                         <td>{index+1}</td>
                         <td>{todo.task.title}</td>
                         <td> {todo.task.description} </td>
                         <td><Checkbox/></td>
-                        <td><Button bsStyle="danger">Del</Button></td>
+                        <td><Button bsStyle="danger" onClick={()=>{this.delTask(`${todo.id}`)}}> Del</Button></td>
                     </tr>
                 })}
                 </tbody>
