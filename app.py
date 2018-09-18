982b732c969c0466eef9a426b59ce61010d4a582
mjesar <mohammadalijaisar@gmail.com>
2018-09-18 19:57:44 +0500

    Get all data route file to fatch complete api from database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, make_response
 
+#importing database model class
 from databaseModel import db
+
+#importing routes with blueprints
+from Routes.get_all import getall_api
+
 
 
 
@@ -11,6 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aliali@localhost/todo'
 db.init_app(app)
 
+
+#create blueprints
+app.register_blueprint(getall_api)
 
 
 if __name__ == '__main__':
