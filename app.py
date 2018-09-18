4c78ee861749483da6c7dfcd42e12729afd9f711
syedmusharrafalam <syedmusharrafalam94@gmail.com>
2018-09-18 23:28:28 +0500

    Main file for access only one selected data

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from Routes.getdata import getdata_api
+from Routes.getone import getone_data
 
 app = Flask(__name__)
 
-
+app.register_blueprint(getone_data)
 app.register_blueprint(getdata_api)
 app.run(debug=True, use_reloader=False, port=1000)
