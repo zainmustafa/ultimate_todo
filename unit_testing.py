9f3a395ae71a420983f92b150d164fa4aae71de4
syedmusharrafalam <syedmusharrafalam94@gmail.com>
2018-09-19 16:29:28 +0500

    testing of create todo for insert data

--- unit_testing.py
+++ unit_testing.py
@@ -21,9 +21,14 @@ class TodoData(unittest.TestCase):
         response = self.app.get('/todoapp/api/v1.0/task/alldata')
         self.assertEqual(response.status_code,200 )
     
-     def test_get_single_todo_data(self):
+    def test_get_single_todo_data(self):
         response =self.app.get('/todoapp/api/v1.0/task/one/5')
         self.assertEqual(response.status_code,200)
+
+    def test_create_todo(self):
+        response = self.app.post('/todoapp/api/v1.0/task/add',data=json.dumps(dict({'title':'javafdsags', 'description' : 'androidsss'})), content_type='application/json')
+        self.assertEqual(response.status_code,200)
+
 
 
 if __name__ == "__main__":
