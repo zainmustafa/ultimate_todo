16c19c4e5eefe6d3eaee9be52cc0bf6fe9fc2965
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2018-09-19 12:48:13 +0500

    create add data blue print

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from Routes.getdata import getdata_api
 from Routes.getone import getone_data
+from Routes.create import create_data_api
+
 
 app = Flask(__name__)
 
 app.register_blueprint(getone_data)
 app.register_blueprint(getdata_api)
+app.register_blueprint(create_data_api)
 app.run(debug=True, use_reloader=False, port=1000)
