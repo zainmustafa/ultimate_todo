5ca4d453321882649e93544f5a2dbec0eaebef0d
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2018-09-19 15:56:35 +0500

    import update from app file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from Routes.getone import getone_data
 from Routes.create import create_data_api
 from Routes.remove import remove_api
 from Routes.change import change_api
+from Routes.update import update_api
 
 
 app = Flask(__name__)
@@ -13,4 +14,6 @@ app.register_blueprint(getdata_api)
 app.register_blueprint(create_data_api)
 app.register_blueprint(remove_api)
 app.register_blueprint(change_api)
+app.register_blueprint(update_api)
+
 app.run(debug=True, use_reloader=False, port=1000)
