2a97d8933190e90b49404757f3cc33f5763895d0
syedmusharrafalam <syedmusharrafalam94@gmail.com>
2018-09-19 13:38:05 +0500

    remove data blue print for remove from database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from Routes.getdata import getdata_api
 from Routes.getone import getone_data
 from Routes.create import create_data_api
+from Routes.remove import remove_api
 
 
 app = Flask(__name__)
@@ -9,4 +10,5 @@ app = Flask(__name__)
 app.register_blueprint(getone_data)
 app.register_blueprint(getdata_api)
 app.register_blueprint(create_data_api)
+app.register_blueprint(remove_api)
 app.run(debug=True, use_reloader=False, port=1000)
