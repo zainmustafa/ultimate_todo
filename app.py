0965b2e0856e0a21d4f5bac2a0b1df9939394b5f
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2018-09-19 15:08:45 +0500

    import change method

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from Routes.getdata import getdata_api
 from Routes.getone import getone_data
 from Routes.create import create_data_api
 from Routes.remove import remove_api
+from Routes.change import change_api
 
 
 app = Flask(__name__)
@@ -11,4 +12,5 @@ app.register_blueprint(getone_data)
 app.register_blueprint(getdata_api)
 app.register_blueprint(create_data_api)
 app.register_blueprint(remove_api)
+app.register_blueprint(change_api)
 app.run(debug=True, use_reloader=False, port=1000)
