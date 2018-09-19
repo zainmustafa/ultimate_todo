8e9780573151b3fb1d81568a90633dff9a762f5d
Osama Khan <osama.ahmedkhan96@gmail.com>
2018-09-19 12:43:56 +0500

    enable cors

--- lib/app.ts
+++ lib/app.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/todo";
 import * as mongoose from "mongoose";
-
+import  * as cors from "cors";
 class App {
 
     public app: express.Application;
@@ -10,6 +10,7 @@ class App {
     public mongoUrl: string = 'mongodb://osamakhan:teamgamma3@ds257372.mlab.com:57372/ultimate-todo-list';
     constructor() {
         this.app = express();
+        this.app.use(cors());
         this.config();
         this.routePrv.routes(this.app);
         this.mongoSetup();
