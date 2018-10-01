09d29d7671bbfe73914c4f84a1d4e1f6a4080bc8
Saad Pasta <saadpasta70@gmail.com>
2018-10-01 13:27:46 +0500

    Api Add And Show Functionlity Added

---
+++ Grpc Client Api/src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import './App.css'; 
+import AddForm from "./Components/AddForm/AddForm";
+import TaskList from "./Components/TaskList/TaskList";
+import logo from './logo.png';
+import swal from 'sweetalert2';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            taskTxt:'',
+            description:'',
+        };
+        this.add  = this.add.bind(this);
+        this.delTask  = this.delTask.bind(this);
+        this.navabar=this.navabar.bind(this);
+        this.updateTask=this.updateTask.bind(this)
+    }
+    componentWillMount(){
+        fetch('http://localhost:2000/todo/api/v1.0/tasks/').then(res => res.json())
+            .then(response => console.log('res', this.setState({todos:response})))
+            .catch(error => console.error('Error:', error));
+    }
+
+    add(task, description){
+        const {todos} = this.state;
+        fetch('http://localhost:2000/todo/api/v1.0/tasks/', {
+            method: 'POST',
+            body: JSON.stringify({title: task,description}),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(response => {
+                todos.push(response);
+                this.setState({todos})
+            })
+            .catch(error => console.error('Error:', error));
+    }
+    delTask(id,index){
+        const {todos} = this.state;
+        fetch(`https://http://localhost:2000/todo/api/v1.0/tasks/${id}`,{method: "DELETE"})
+            .then(res => res.json())
+            .then(response => {
+            })
+    }
+    updateTask(id,index){
+        const {todos} = this.state;
+        swal({
+            title: 'Firebase Realtime Todo',
+            html:
+            '<h2>Update Your Todo</h2>'+
+            '<input id="swal-input1" class="swal2-input" value="'+todos[index].title+'" autofocus placeholder="Title" >' +
+            '<input id="swal-input2" class="swal2-input" value="'+todos[index].description+'" placeholder="Description" >',
+            preConfirm: function() {
+                return new Promise(function(resolve) {
+                    if (true) {
+                        resolve([
+                            document.getElementById('swal-input1').value,
+                            document.getElementById('swal-input2').value
+                        ]);
+                    }
+                });
+            }
+        }).then((result) => {
+            fetch(`http://localhost:2000/todo/api/v1.0/tasks/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({title: result.value[0],
+                    description: result.value[1]}),
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+                .then(response => {
+                    todos[index] = response;
+                    this.setState({todos});
+                    swal(
+                        'Firebase Realtime Todo!',
+                        'Your Todo Has Been Updated!',
+                        'success'
+                    );
+                }).catch(error => console.error('Error:', error));
+        })
+    }
+    render() {
+        const {todos} = this.state;
+        return (
+            <div className="App">
+                <this.navabar/>
+                <div className="row">
+                    <div className="col-md-8 mx-auto">
+                        <div className="form-group">
+                            <AddForm Add={this.add}/>
+                        </div>
+                        <div className="form-group">
+                            {todos && <TaskList updateTask={this.updateTask} updateDone={this.updateDone} delTask={this.delTask} todoList={todos}/>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+  }
+
+  navabar() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          <img src={logo} width="35" height="35" className="d-inline-block align-middle" alt=""/>
+            Firebase Realtime Todo
+        </a>
+      </nav>
+    )
+  }
+}
+
+export default App;
---
+++ Grpc Client Api/src/Components/TaskList/TaskList.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+
+class TaskList extends Component{
+
+    constructor(porps){
+        super(porps);
+    }
+
+    render(){
+        const {todoList} = this.props;
+        console.log(todoList)
+        return(
+            <div className="table-responsive">
+            <table className="table">
+                <thead className="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Task</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Done</th>
+                    <th scope="col" colSpan={2} className="text-center">Options</th>
+                </tr>
+                </thead>
+                
+               <tbody className="todolistmain" >
+                {todoList.employee.map((todo, index) => {
+                    console.log(todo.Todoid)
+                    return <tr key={todo.Todoid}>
+                        <td scope="row">{index+1}</td>
+                        <td>{todo.title}</td>
+                        <td> {todo.description} </td>
+                        <td>
+                        </td>
+                        <td><button className="btn btn-sm btn-info" onClick={()=>{this.props.updateTask(`${todo.Todoid}`,`${index}`)}}>Edit</button></td>
+                        <td><button className="btn btn-sm btn-danger" onClick={()=>{this.props.delTask(`${todo.Todoid}`,`${index}`)}}> Delete</button></td>
+                    </tr>
+                })}
+                </tbody>
+            </table>
+            </div>
+        );
+    }
+}
+
+export default TaskList;
